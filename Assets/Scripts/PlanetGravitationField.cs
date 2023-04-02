53ed0e54d8a5ff2994397a311159d84c440af7a5
qox701 <765983658@qq.com>
2023-04-02 19:26:29 +0800

    Gravitation fixed edition

--- Assets/Scripts/PlanetGravitationField.cs
+++ Assets/Scripts/PlanetGravitationField.cs
@@ -8,9 +8,8 @@ public class PlanetGravitationField : MonoBehaviour
 {
     public static readonly float FieldRatio = 8f;
 
-    private const float GravitationFactor = 0.1f;
-
-    private Vector3 _previousGravitation;
+    private const float GravitationFactor = 3f;
+    
     private float _mass;
 
     private void OnEnable()
@@ -34,20 +33,13 @@ public class PlanetGravitationField : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         PlanetController planet = other.GetComponent<PlanetController>();
-        if (planet) { //Calculate gravitation
-            Vector3 gravitationVector = planet.transform.position - this.transform.position;
-            Vector3 currentGravitation = (gravitationVector.normalized) *
-                                         (GravitationFactor * _mass * Mathf.Pow(gravitationVector.magnitude, 2f));
-             planet.gravitation -= (currentGravitation - _previousGravitation);
-             _previousGravitation = currentGravitation;
-             return;}
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        PlanetController planet = other.GetComponent<PlanetController>();
-         if (planet) { 
-             planet.gravitation += _previousGravitation;
+        if (planet) { 
+            Vector3 gravitationVector = -(planet.transform.position - this.transform.position);//Vector from player to this planet
+            Vector3 gravitation = (gravitationVector.normalized) *
+                                         (GravitationFactor * _mass / Mathf.Pow(gravitationVector.magnitude, 2f));//Calculate gravitation
+             
+             planet.gravitation = gravitation; //Set gravitation
+             
              return;}
     }
 }
