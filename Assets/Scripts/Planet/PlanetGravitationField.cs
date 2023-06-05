f387b66d9744191284c1127f52090f54c8329bc8
hayahane <02888761169@189.cn>
2023-06-05 11:10:32 +0800

    perf(PlanetGravitationField): 修改引力应用方式，减少耦合，优化程序集依赖

--- Assets/Scripts/Planet/PlanetGravitationField.cs
+++ Assets/Scripts/Planet/PlanetGravitationField.cs
@@ -1,5 +1,5 @@
-using Flawless.PlayerCharacter;
 using UnityEngine;
+using Object = System.Object;
 
 namespace Flawless.Planet
 {
@@ -10,17 +10,13 @@ namespace Flawless.Planet
         private const float GravitationFactor = 5f;
         private const float GravitationPower = 1.5f;
 
-        private float _mass;
-
-        private Vector3 _prevGrav;
-
-        private PlanetController _planet;
+        private Rigidbody _rigidbody;
 
         #region MonoBehaviours
 
         private void OnEnable()
         {
-            _mass = GetComponent<Rigidbody>().mass;
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
         #endregion
@@ -29,35 +25,20 @@ namespace Flawless.Planet
 
         private void OnTriggerStay(Collider other)
         {
-            // Returns if the object is not a player
-            if (!other.CompareTag("Player")) return;
-
-            // Get the PlanetController Component of the other object
-            _planet = other.GetComponent<PlanetController>();
-            if (!_planet) return; // Return if null
-
+            var inFieldRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            if (inFieldRigidbody == null || inFieldRigidbody.isKinematic || inFieldRigidbody.gameObject == this.gameObject)
+                return;
+            
+            Debug.Log(other.transform.root);
             Vector3 gravitationVector =
-                this.transform.position - _planet.transform.position; //Vector from player to this planet
+                this.transform.position - other.transform.position; //Vector from player to this planet
 
             // TODO: Adjust Gravitation Calculation to have a more interesting movement controller
             Vector3 gravitation = (gravitationVector.normalized) *
-                                  (GravitationFactor * _mass /
+                                  (GravitationFactor * _rigidbody.mass /
                                    Mathf.Pow(gravitationVector.magnitude, GravitationPower)); //Calculate Gravitation
-
-            _planet.Gravitation += gravitation - _prevGrav; //Set Gravitation
-            _prevGrav = gravitation;
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            // Returns if the object is not a player
-            if (!other.CompareTag("Player")) return;
-
-            _planet = other.GetComponent<PlanetController>();
-            if (!_planet) return; // Return if null
-
-            _planet.Gravitation -= _prevGrav;
-            _prevGrav = Vector3.zero;
+            
+            inFieldRigidbody.AddForce(gravitation, ForceMode.Acceleration); //Apply Gravitation
         }
 
         #endregion
