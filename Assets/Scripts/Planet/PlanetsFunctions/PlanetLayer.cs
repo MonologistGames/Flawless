0e2af2c178235cf5ca368e7f1b6e7d947730d272
qox701 <765983658@qq.com>
2023-06-06 23:10:58 +0800

    add(Scripts/Prefabs):增加了两个新星球的功能

--- Assets/Scripts/Planet/PlanetsFunctions/PlanetLayer.cs
+++ Assets/Scripts/Planet/PlanetsFunctions/PlanetLayer.cs
@@ -8,41 +8,23 @@ using Unity.VisualScripting;
 namespace Flawless.Planet.PlanetsFunctions
 {
     [RequireComponent(typeof(GravitationField))]
-    public class PlanetAir : PlanetLife
+    public class PlanetLayer : PlanetLife
     {
         private GravitationField _gravitationField;
         private float _mass;
         
-        private float _nowScale;
-        private float _atmosScale;
-        [Header("Atmos")]
-        public Transform Atmos;
-        [Header("ShrinkVale")]
-        public float objectiveScale=0.6f;
-        public float shrinkSpeed = 0.1f;
         
-        private bool isDead=false;
         public void Start()
         {
             _gravitationField = GetComponent<GravitationField>();
             _mass = _gravitationField.Mass;
-            
-            _nowScale= this.transform.localScale.x;
-            _atmosScale = Atmos.localScale.x;
         }
 
-        public void Update()
-        {
-            if (isDead && _nowScale > objectiveScale)
-            {
-                Shrink();
-            }
-        }
+        
 
         public override void SetPlanetDead()
         {
-            _gravitationField.Mass = _mass * 2;
-            isDead = true;
+            _gravitationField.Mass = _mass * 1.5f;
         }
 
         public override void CollideAndDamageLife(Rigidbody playerRigidbody, PlayerLife playerLife, Vector3 normal)
@@ -57,12 +39,6 @@ namespace Flawless.Planet.PlanetsFunctions
             playerLife.CollideAndDamageLife(CollideDamage);
         }
 
-        private void Shrink()
-        {
-            _nowScale-= shrinkSpeed*Time.unscaledDeltaTime;
-            this.transform.localScale = new Vector3(_nowScale,_nowScale,_nowScale);
-            _atmosScale-= shrinkSpeed*Time.unscaledDeltaTime;
-            Atmos.localScale = new Vector3(_atmosScale,_atmosScale,_atmosScale);
-        }
+        
     }
 }
